8c1b58513cb0791bc25d8eb2395c440018a7036b
Alex <gan555@mail.ru>
2025-12-30 16:35:34 +0300

    make some locators

---
+++ utils/locators.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators(object):
+    HEADER_MENU = (By.CLASS_NAME, 'Header_links__CblPJ menu ')
+    PROJECTS = (By.LINK_TEXT, 'Header_links__CblPJ menu ')
+    COMPANY
+    FIELDS
+    BLOG
+    CONTACTS
+
+
+class FooterLocators(object):
+    ROOT =
+    START_PROJECT_BUTTON =
+    LOGO =
+    SOCIALS =
+    TELEGRAM =
+    CONTACT =
+    TEXT =
+    DOCUMENTS =
+    YEAR =
+    COPYRIGHTS =
+    PRIVACY =
